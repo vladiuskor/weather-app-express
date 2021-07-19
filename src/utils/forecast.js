2d5170b3e51bf527824ea0173f9caf22ca38f868
Vlad Korobko <max.vlad.kor@gmail.com>
2021-07-19 16:52:28 +0300

    First commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=45d9771c39f6013087122f7569bde4a6&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude);
+
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if(body.error) {
+            callback('Unable to  find location!', undefined);
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.')
+        }
+
+    })
+}
+
+
+module.exports = forecast;
